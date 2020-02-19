0001ee3b1b1bcf7057dfeaf905a663f91a84a9ab
Dan Gunnarsson <dangunnarsson@Dans-MacBook-Pro.local>
2020-02-19 21:09:50 +0100

    Updated process_data.py with docstrings

--- data/process_data.py
+++ data/process_data.py
@@ -1,3 +1,15 @@
+"""
+Script to load and pre-process data
+
+Sample script execution:
+python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+
+Arguments:
+    1) Messages file (disaster_messages.csv)
+    2) Categories file (disaster_categories.csv)
+    3) SQL Database to output the pre-processed data (DisasterResponse.db)
+"""
+
 import sys
 import pandas as pd
 import numpy as np
@@ -5,15 +17,40 @@ import sqlite3
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    #load messages and categories data sets
+    """Loads the messages and categories data
+
+    Parameters
+    ----------
+    messages_filepath : str
+        The message file including path
+    categories_filepath : str
+        The categories file including path
+
+    Returns
+    -------
+    df : DataFrame
+        The loaded message and categories data merged to a Pandas DataFrame
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages,categories,on='id')
     return df
 
 def clean_data(df):
-    # create a dataframe of the 36 individual category columns
+    """Clean the DataFrame by dividing categories in to separate columns,
+    removing duplicates and removing colums and rows with NaN values.
 
+    Parameters
+    ----------
+    df : DataFrame
+        The loaded message and categories data merged to a Pandas DataFrame
+
+    Returns
+    -------
+    df : DataFrame
+        The cleaned data frame with loaded message and categories data
+    """
+    # create a dataframe of the 36 individual category columns
     categories = df.categories.str.split(";",expand=True)
 
     # select the first row of the categories dataframe
@@ -49,10 +86,28 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
+    """Save the dataframe with messages and categories data to a SQLlite
+    database.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The loaded message and categories data merged to a Pandas DataFrame
+    database_filename : str
+        The loaded message and categories data merged to a Pandas DataFrame
+    """
     engine = create_engine('sqlite:///'+ database_filename)
     df.to_sql('InsertTableName', engine, index=False)
 
 def main():
+    """
+    Main function
+
+    1) Read command line arguments
+    2) Load data
+    3) Clean data
+    4) Save data to SQLite database
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
